64b9a5edd83c4e91f7b33ffa9c8e61903d4ae25d
Erin Claudio <eclaudio@gmail.com>
2020-09-11 16:04:06 -0400

    New Article Errors in title and description created

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -8,12 +8,15 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 end
